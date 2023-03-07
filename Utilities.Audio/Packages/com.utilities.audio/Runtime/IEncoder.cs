e7dabb9f5f96b6f944b36a9dd115bc837c7af4ef
Stephen Hodgson <hodgson.designs@gmail.com>
2023-03-07 16:19:54 -0500

    com.utilities.audio 1.0.0-preview.2
    fixed missing script changes
    fixed readme docs

--- Utilities.Audio/Packages/com.utilities.audio/Runtime/IEncoder.cs
+++ Utilities.Audio/Packages/com.utilities.audio/Runtime/IEncoder.cs
@@ -1,6 +1,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -9,6 +10,6 @@ namespace Utilities.Audio
 {
     public interface IEncoder
     {
-        Task<Tuple<string, AudioClip>> StreamSaveToDiskAsync(AudioClip clip, string saveDirectory, CancellationToken cancellationToken, Action<Tuple<string, AudioClip>> callback = null);
+        Task<Tuple<string, AudioClip>> StreamSaveToDiskAsync(AudioClip clip, string saveDirectory, CancellationToken cancellationToken, Action<Tuple<string, AudioClip>> callback = null, [CallerMemberName] string callingMethodName = null);
     }
 }
