cbd1d7851e57f336826fbde699687d3514d84a8e
Stephen Hodgson <hodgson.designs@gmail.com>
2023-03-07 16:13:50 -0500

    com.utilities.audio 1.0.0-preview.1 (#1)

---
+++ Utilities.Audio/Packages/com.utilities.audio/Runtime/Constants.cs
@@ -0,0 +1,9 @@
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Utilities.Audio
+{
+    public static class Constants
+    {
+        public const float RescaleFactor = 32768f;
+    }
+}
