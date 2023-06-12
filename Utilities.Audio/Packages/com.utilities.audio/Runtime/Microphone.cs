2d5509ae08214061552ea9d90143912599467a6f
Stephen Hodgson <hodgson.designs@gmail.com>
2023-06-12 04:19:49 -0400

    added Microphone.jslib
    updated Microphone.cs with WebGL bindings

--- Utilities.Audio/Packages/com.utilities.audio/Runtime/Microphone.cs
+++ Utilities.Audio/Packages/com.utilities.audio/Runtime/Microphone.cs
@@ -3,9 +3,11 @@
 using System;
 using UnityEngine;
 
-#if UNITY_WEBGL
+#if UNITY_WEBGL //&& !UNITY_EDITOR
+using AOT;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 #endif
 
 namespace Utilities.Audio
@@ -15,27 +17,63 @@ namespace Utilities.Audio
     /// </summary>
     public class Microphone
     {
-#if UNITY_WEBGL
-        // TODO Implement WebGL callbacks
+#if UNITY_WEBGL //&& !UNITY_EDITOR
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        private static void Init()
+        {
+            InitMicrophoneJS();
+        }
 
-        private static int GetNumberOfMicrophones() { return 0; }
+        #region Interop
 
-        private static string GetMicrophoneDeviceName(int index) { return null; }
+        [DllImport("__Internal")]
+        private static extern void InitMicrophoneJS();
 
+        [DllImport("__Internal")]
+        private static extern int GetNumberOfMicrophonesJS();
+
+        [DllImport("__Internal")]
+        private static extern string GetMicrophoneDeviceNameJS(int index);
+
+        [DllImport("__Internal")]
+        private static extern void StartRecordingJS(Action<int, IntPtr> callback, float[] buffer, int bufferSize);
+
+        [DllImport("__Internal")]
+        private static extern void StopRecordingJS();
+
+        [DllImport("__Internal")]
+        private static extern bool IsRecordingJS();
+
+        #endregion Interop
+
+        private static bool loop;
+        private static int currentPosition;
+        private static int frequency = 41000;
+        private static float[] audioBuffer;
+        private static AudioClip currentClip;
         private static readonly HashSet<string> deviceList = new HashSet<string>();
 #endif
+        private static bool isRecording;
 
+        /// <summary>
+        /// A list of available microphone devices, identified by name.
+        /// </summary>
+#pragma warning disable IDE1006
         // ReSharper disable once InconsistentNaming
         public static string[] devices
+#pragma warning restore IDE1006
         {
-#if UNITY_WEBGL
+#if UNITY_WEBGL //&& !UNITY_EDITOR
             get
             {
-                var size = GetNumberOfMicrophones();
+                var size = GetNumberOfMicrophonesJS();
 
                 for (var index = 0; index < size; ++index)
                 {
-                    var deviceName = GetMicrophoneDeviceName(index);
+                    var deviceName = GetMicrophoneDeviceNameJS(index);
+
+                    if (string.IsNullOrWhiteSpace(deviceName)) { continue; }
+
                     deviceList.Add(deviceName);
                 }
 
@@ -46,64 +84,140 @@ namespace Utilities.Audio
 #endif
         }
 
+        /// <summary>
+        /// Start Recording with device.
+        /// </summary>
+        /// <param name="deviceName">The name of the device.</param>
+        /// <param name="loop">Indicates whether the recording should continue recording if lengthSec is reached, and wrap around and record from the beginning of the AudioClip.</param>
+        /// <param name="lengthSec">Is the length of the AudioClip produced by the recording.</param>
+        /// <param name="frequency">The sample rate of the AudioClip produced by the recording.</param>
+        /// <returns>
+        /// The function returns null if the recording fails to start.
+        /// </returns>
         public static AudioClip Start(string deviceName, bool loop, int lengthSec, int frequency)
         {
             switch (lengthSec)
             {
-                case <= 0:
-                    throw new ArgumentException($"Length of recording must be greater than zero seconds (was: {lengthSec} seconds)");
-                case > 3600:
-                    throw new ArgumentException($"Length of recording must be less than one hour (was: {lengthSec} seconds)");
+                case <= 0: throw new ArgumentException($"Length of recording must be greater than zero seconds (was: {lengthSec} seconds)");
+                case > 3600: throw new ArgumentException($"Length of recording must be less than one hour (was: {lengthSec} seconds)");
             }
 
-            if (frequency <= 0)
+            if (frequency <= 0) { throw new ArgumentException($"Frequency of recording must be greater than zero (was: {frequency} Hz)"); }
+
+            if (isRecording)
             {
-                throw new ArgumentException($"Frequency of recording must be greater than zero (was: {frequency} Hz)");
+                Debug.LogError("A recording session is already in progress!");
+                return null;
             }
 
-#if UNITY_WEBGL
-            throw new NotImplementedException();
+            isRecording = true;
+#if UNITY_WEBGL //&& !UNITY_EDITOR
+            Microphone.loop = loop;
+            Microphone.frequency = frequency;
+            var channels = 1;
+            currentPosition = 0;
+            audioBuffer = new float[frequency * lengthSec /* * channels*/];
+            StartRecordingJS(StreamCallback, audioBuffer, audioBuffer.Length);
+            currentClip = AudioClip.Create("WebMic_Recording", frequency * lengthSec, channels, frequency, false);
+            return currentClip;
 #else
             return UnityEngine.Microphone.Start(deviceName, loop, lengthSec, frequency);
 #endif
         }
 
+        /// <summary>
+        /// Stops recording.
+        /// </summary>
+        /// <param name="deviceName">The name of the device.</param>
         public static void End(string deviceName)
         {
-#if UNITY_WEBGL
-            throw new NotImplementedException();
+            if (!isRecording) { return; }
+            isRecording = false;
+#if UNITY_WEBGL //&& !UNITY_EDITOR
+            StopRecordingJS();
+            currentClip = null;
 #else
             UnityEngine.Microphone.End(deviceName);
 #endif
         }
 
+        /// <summary>
+        /// Query if a device is currently recording.
+        /// </summary>
+        /// <param name="deviceName">The name of the device.</param>
+        /// <returns></returns>
         public static bool IsRecording(string deviceName)
         {
-#if UNITY_WEBGL
-            throw new NotImplementedException();
+#if UNITY_WEBGL //&& !UNITY_EDITOR
+            return IsRecordingJS();
 #else
             return UnityEngine.Microphone.IsRecording(deviceName);
 #endif
         }
 
+        /// <summary>
+        /// Get the frequency capabilities of a device.
+        /// </summary>
+        /// <param name="deviceName">The name of the device.</param>
+        /// <param name="minFreq">Returns the minimum sampling frequency of the device.</param>
+        /// <param name="maxFreq">Returns the maximum sampling frequency of the device.</param>
         public static void GetDeviceCaps(string deviceName, out int minFreq, out int maxFreq)
         {
-            minFreq = 0;
-            maxFreq = 0;
-#if UNITY_WEBGL
-            throw new NotImplementedException();
+            // When a value of zero is returned in the minFreq and maxFreq parameters,
+            // this indicates that the device supports any frequency.
+#if UNITY_WEBGL //&& !UNITY_EDITOR
+            minFreq = frequency;
+            maxFreq = frequency;
 #else
             UnityEngine.Microphone.GetDeviceCaps(deviceName, out minFreq, out maxFreq);
 #endif
         }
 
+        /// <summary>
+        /// Get the position in samples of the recording.
+        /// </summary>
+        /// <param name="deviceName">The name of the device.</param>
         public static int GetPosition(string deviceName)
         {
-#if UNITY_WEBGL
-            throw new NotImplementedException();
+            if (!isRecording) { return 0; }
+#if UNITY_WEBGL //&& !UNITY_EDITOR
+            return currentPosition;
 #else
             return UnityEngine.Microphone.GetPosition(deviceName);
 #endif
         }
+
+#if UNITY_WEBGL //&& !UNITY_EDITOR
+
+        [MonoPInvokeCallback(typeof(Action<int, IntPtr>))]
+        private static void StreamCallback(int size, IntPtr dataPtr)
+        {
+            var samplingData = new float[size];
+            Marshal.Copy(dataPtr, samplingData, 0, size);
+
+            foreach (var sample in samplingData)
+            {
+                audioBuffer[currentPosition] = sample;
+                currentPosition++;
+
+                if (currentPosition == audioBuffer.Length)
+                {
+                    if (loop)
+                    {
+                        currentPosition = 0;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (currentClip != null)
+            {
+                currentClip.SetData(audioBuffer, 0);
+            }
+        }
+#endif
     }
 }
---
+++ Utilities.Audio/Packages/com.utilities.audio/Runtime/Plugins/WebGL/Microphone.jslib
@@ -0,0 +1,135 @@
+var Microphone = {
+  InitMicrophoneJS: function() {
+    if (navigator.mediaDevices === undefined || navigator.mediaDevices.enumerateDevices === undefined) {
+      alert('WebGLMicrophone is not supported in this browser!');
+      return;
+    }
+
+    const userMediaConstraints = {
+      video: false,
+      audio: {
+        channelCount: 2,
+      }
+    };
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    navigator.mediaDevices.getUserMedia(userMediaConstraints).then(_ => {
+      console.log("WebGLMicrophone permissions granted!");
+      document.microphoneContext = new Object();
+      document.microphoneContext.devices = [];
+      document.microphoneContext.position = 0;
+      document.microphoneContext.isRecording = false;
+      document.microphoneContext.audioContext = new AudioContext({"sampleRate": 44100});
+
+      navigator.mediaDevices.enumerateDevices().then(function(devices) {
+        document.microphoneContext.devices = [];
+        devices.forEach(function(device) {
+          console.log(`${device.kind}: ${device.label} id=${device.deviceId}`);
+
+          if (device.kind === 'audioinput') {
+            document.microphoneContext.devices.push(device.label);
+          }
+        });
+      }).catch(function(error) {
+        console.error(`${error.name}: ${error.message}`);
+      });
+    }).catch(function(error) {
+      console.error(`${error.name}: ${error.message}`);
+    });
+
+    setInterval(function() {
+      if (document.microphoneContext === undefined) {
+        console.log("Waiting for WebGLMicrophone permissions")
+        return;
+      }
+
+      var audioContext = document.microphoneContext.audioContext;
+
+      if (audioContext.state === "suspended" || audioContext.state === "interrupted") {
+        console.log(`audioContext.state::changed -> ${audioContext.state}`);
+        audioContext.resume();
+      }
+    }, 300);
+  },
+
+  GetNumberOfMicrophonesJS: function() {
+    if (document.microphoneContext == undefined || document.microphoneContext.devices === undefined) {
+      return 0;
+    }
+
+    return document.microphoneContext.devices.length;
+  },
+
+  GetMicrophoneDeviceNameJS: function(index) {
+    if (document.microphoneContext == undefined || document.microphoneContext.devices === undefined) {
+      return null;
+    }
+
+    var devices = document.microphoneContext.devices;
+
+    if (index >= 0 && index < devices.length) {
+      var deviceName = devices[index];
+      var bufferSize = lengthBytesUTF8(deviceName) + 1;
+      var buffer = _malloc(bufferSize);
+      stringToUTF8(deviceName, buffer, bufferSize);
+      return buffer;
+    }
+
+    return null;
+  },
+
+  StartRecordingJS: function(functionPtr, bufferPtr, bufferSize) {
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ audio: true }).then(function(stream) {
+        var audioContext = document.microphoneContext.audioContext;
+        var source = audioContext.createMediaStreamSource(stream);
+        const numberOfInputChannels = 1; // Mono audio
+        const numberOfOutputChannels = 1;
+
+        if (audioContext.createScriptProcessor) {
+          document.microphoneContext.recorder = audioContext.createScriptProcessor(bufferSize, numberOfInputChannels, numberOfOutputChannels);
+        } else {
+          document.microphoneContext.recorder = audioContext.createJavaScriptNode(bufferSize, numberOfInputChannels, numberOfOutputChannels);
+        }
+
+        document.microphoneContext.recorder.onaudioprocess = function (stream) {
+          var buffer = stream.inputBuffer.getChannelData(0);
+          var f32Buffer = new Float32Array(buffer);
+          HEAPF32.set(f32Buffer, bufferPtr >> 2); // using HEAPF32 and right shift 2
+          Module.dynCall_vii(functionPtr, buffer.length, bufferPtr);
+        };
+
+        source.connect(document.microphoneContext.recorder);
+        document.microphoneContext.recorder.connect(audioContext.destination);
+        document.microphoneContext.source = source;
+        document.microphoneContext.isRecording = true;
+
+        console.log("WebGLMicrophone started recording");
+      }).catch(function(error) {
+          console.log(`Failed in GetUserMedia: ${error}`);
+      });
+    }
+  },
+
+  StopRecordingJS: function() {
+    if (document.microphoneContext == undefined) {
+      console.error("WebGLMicrophone not initialized!");
+      return;
+    }
+
+    document.microphoneContext.source.disconnect(document.microphoneContext.recorder);
+    document.microphoneContext.source = null;
+    document.microphoneContext.recorder.disconnect();
+    document.microphoneContext.recorder = null;
+    document.microphoneContext.isRecording = false;
+  },
+
+  IsRecordingJS: function() {
+    if (document.microphoneContext == undefined) {
+      return false;
+    }
+
+    return document.microphoneContext.isRecording === true;
+  }
+}
+
+mergeInto(LibraryManager.library, Microphone);
