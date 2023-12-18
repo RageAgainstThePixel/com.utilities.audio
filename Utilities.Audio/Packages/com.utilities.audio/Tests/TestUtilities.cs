ef732a2877227ff2e36917ef75fe5f3d8a719c81
Stephen Hodgson <hodgson.designs@gmail.com>
2023-12-18 18:15:44 -0500

    com.utilities.audio 1.0.13
    - updated unit tests
    - updated PCMEncoder

--- Utilities.Audio/Packages/com.utilities.audio/Tests/TestUtilities.cs
+++ Utilities.Audio/Packages/com.utilities.audio/Tests/TestUtilities.cs
@@ -6,49 +6,74 @@ namespace Utilities.Audio.Tests
 {
     internal static class TestUtilities
     {
-        public static float[] GenerateSineWaveSamples(double frequency, int sampleRate)
+        public static float[] GenerateSineWaveSamples(double frequency, int sampleRate, float duration = 1f, float amplitude = 0.5f)
         {
-            var samples = new float[sampleRate]; // Only create samples for the sine wave
-            var samplePeriod = 1.0 / sampleRate;
+            var sampleCount = (int)(sampleRate * duration);
+            var samples = new float[sampleCount];
 
-            // Generate the actual sine wave
-            for (var i = 0; i < sampleRate; i++)
+            for (var i = 0; i < sampleCount; i++)
             {
-                var time = i * samplePeriod;
-                var sineValue = Math.Sin(2 * Math.PI * frequency * time);
-                samples[i] = (float)sineValue;
+                var period = i / (double)sampleRate;
+                var value = amplitude * Math.Sin(2 * Math.PI * frequency * period);
+                samples[i] = (float)value;
             }
 
+            NormalizeSamples(ref samples);
             return samples;
         }
 
-        public static float[] GenerateSineWaveSamplesWithSilence(double frequency, int sampleRate, int silenceSamplesAtStart, int silenceSamplesAtEnd)
-        {    // Assuming sampleRate is the number of non-silent samples excluding any silence
-            var totalSampleCount = sampleRate + silenceSamplesAtStart + silenceSamplesAtEnd;
-            var samples = new float[totalSampleCount];
-            // Total samples including non-silent and silent samples
-            var samplePeriod = 1.0 / sampleRate;
+        public static float[] GenerateSineWaveSamplesWithSilence(double frequency, int sampleRate, int silenceSamplesAtStart, int silenceSamplesAtEnd, float duration = 1f, float amplitude = 0.5f)
+        {
+            // Assuming sampleRate is the number of non-silent samples excluding any silence
+            var sampleCount = (int)(sampleRate * duration) + silenceSamplesAtStart + silenceSamplesAtEnd;
+            var samples = new float[sampleCount];
 
             // Generate silence at the start of the sample
-            for (int i = 0; i < silenceSamplesAtStart; i++)
+            for (var i = 0; i < silenceSamplesAtStart; i++)
             {
                 samples[i] = 0.0f;
             }
 
             // The actual sine wave
-            for (int i = silenceSamplesAtStart; i < sampleRate + silenceSamplesAtStart; i++)
+            for (var i = silenceSamplesAtStart; i < sampleRate + silenceSamplesAtStart; i++)
             {
-                var time = (i - silenceSamplesAtStart) * samplePeriod;
-                samples[i] = (float)Math.Sin(2 * Math.PI * frequency * time);
+                var period = (i - silenceSamplesAtStart) / (double)sampleRate;
+                var value = amplitude * Math.Sin(2 * Math.PI * frequency * period);
+                samples[i] = (float)value;
             }
 
             // Generate silence at the end of the sample
-            for (int i = sampleRate + silenceSamplesAtStart; i < totalSampleCount; i++)
+            for (var i = sampleRate + silenceSamplesAtStart; i < sampleCount; i++)
             {
                 samples[i] = 0.0f;
             }
 
+            NormalizeSamples(ref samples);
             return samples;
+        }
+
+        private static void NormalizeSamples(ref float[] samples)
+        {
+            var maxAbsValue = 0f;
+
+            // Find the maximum absolute value in the samples
+            foreach (var sample in samples)
+            {
+                var absValue = Math.Abs(sample);
+                if (absValue > maxAbsValue)
+                {
+                    maxAbsValue = absValue;
+                }
+            }
+
+            // Normalize the samples based on the maximum absolute value
+            if (maxAbsValue > 1.0f)
+            {
+                for (var i = 0; i < samples.Length; i++)
+                {
+                    samples[i] /= maxAbsValue;
+                }
+            }
         }
     }
 }
